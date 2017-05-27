35e68e54213359733fefa6edb505fcacc0a99d9b
David Ortiz <z.david.ortiz@gmail.com>
2017-05-27 20:41:41 +0200

    apicall: complete to_request()
    Now it should return the correct Request for Authorizes without oauth.
    Authreps and Reports need more work.

--- src/apicall.rs
+++ src/apicall.rs
@@ -28,24 +28,48 @@ pub struct Info<'service, 'app, 'user> {
     user: Option<&'user User>,
 }
 
+// TODO: define endpoints for oauth
+const AUTH_ENDPOINT: &str = "/transactions/authorize.xml";
+const AUTHREP_ENDPOINT: &str = "/transactions/authrep.xml";
+const REPORT_ENDPOINT: &str = "/transactions.xml";
+
 impl<'service, 'app, 'user> Info<'service, 'app, 'user> {
     pub fn new(kind: Type, service: &'service Service, application: &'app Application, user: Option<&'user User>) -> Self {
         Self { kind, service, application, user }
+    }
+
+    fn endpoint(&self) -> &str {
+        use self::Type::*;
+
+        match self.kind {
+            Authorize => AUTH_ENDPOINT,
+            AuthRep => AUTHREP_ENDPOINT,
+            Report => REPORT_ENDPOINT,
+        }
+    }
+
+    fn path(&self) -> String {
+        use request::ToParams;
+
+        let mut params: Vec<(&str, &str)> = Vec::new();
+        params.extend(self.service.to_params().iter());
+        params.extend(self.application.to_params().iter());
+
+        if let Some(user_params) = self.user.as_ref() {
+            params.extend(user_params.to_params().iter());
+        }
+
+        let params = params.iter()
+            .map(|&(param, value)| param.to_owned() + "=" + value)
+            .collect::<Vec<String>>()
+            .join("&");
+
+        self.endpoint().to_owned() + "?" + params.as_str()
     }
 }
 
 impl<'service, 'app, 'user> ToRequest for Info<'service, 'app, 'user> {
     fn to_request(&self) -> Request {
-        use request::ToParams;
-
-        let mut path: String = self.service.to_params();
-
-        path.push_str(self.application.to_params().as_str());
-
-        if let Some(user_params) = self.user.as_ref() {
-            path.push_str(user_params.to_params().as_str());
-        }
-
-        Request::new(self.kind.method(), path, None)
+        Request::new(self.kind.method(), self.path(), None)
     }
 }
