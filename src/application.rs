cc4112a5b17b32b9172617d4d8b681f23020e973
Alejandro Martinez Ruiz <alex@flawedcode.org>
2017-05-31 09:56:39 +0100

    application: avoid stringly types by implementing several traits
    This allows all data to be correctly typed. As we add validation we
    will probably have to move From/Into into TryFrom/TryInto.

--- src/application.rs
+++ src/application.rs
@@ -1,27 +1,172 @@
 use request::ToParams;
+use errors::*;
+
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct AppId(String);
+#[derive(Debug)]
+pub struct AppKey(String);
+#[derive(Debug)]
+pub struct UserKey(String);
+#[derive(Debug)]
+pub struct OAuthToken(String);
+
+// These trait impls provide a way to reference our types as &str
+impl AsRef<str> for AppId {
+    fn as_ref(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl AsRef<str> for AppKey {
+    fn as_ref(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl AsRef<str> for UserKey {
+    fn as_ref(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl AsRef<str> for OAuthToken {
+    fn as_ref(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+// These trait impls provide a way to &str#parse() our Application type
+impl FromStr for AppId {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<AppId> {
+        Ok(AppId(s.to_owned()))
+    }
+}
+
+impl FromStr for AppKey {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<AppKey> {
+        Ok(AppKey(s.to_owned()))
+    }
+}
+
+impl FromStr for UserKey {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<UserKey> {
+        Ok(UserKey(s.to_owned()))
+    }
+}
+
+impl FromStr for OAuthToken {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<OAuthToken> {
+        Ok(OAuthToken(s.to_owned()))
+    }
+}
+
+// These trait impls are similar to FromStr (but are infallible)
+impl<'a> From<&'a str> for AppId where Self: FromStr {
+    fn from(s: &'a str) -> AppId {
+        s.parse().unwrap()
+    }
+}
+
+impl<'a> From<&'a str> for AppKey where Self: FromStr {
+    fn from(s: &'a str) -> AppKey {
+        s.parse().unwrap()
+    }
+}
+
+impl<'a> From<&'a str> for UserKey where Self: FromStr {
+    fn from(s: &'a str) -> UserKey {
+        s.parse().unwrap()
+    }
+}
+
+impl<'a> From<&'a str> for OAuthToken where Self: FromStr {
+    fn from(s: &'a str) -> OAuthToken {
+        s.parse().unwrap()
+    }
+}
+
+// These trait impls take ownership of a given String and also provide Into<AppId> for String
+impl From<String> for AppId {
+    fn from(s: String) -> AppId {
+        AppId(s)
+    }
+}
+
+impl From<String> for AppKey {
+    fn from(s: String) -> AppKey {
+        AppKey(s)
+    }
+}
+
+impl From<String> for UserKey {
+    fn from(s: String) -> UserKey {
+        UserKey(s)
+    }
+}
+
+impl From<String> for OAuthToken {
+    fn from(s: String) -> OAuthToken {
+        OAuthToken(s)
+    }
+}
 
 #[derive(Debug)]
 pub enum Application {
-    AppId(String, Option<String>),
-    UserKey(String),
-    OAuthToken(String)
+    AppId(AppId, Option<AppKey>),
+    UserKey(UserKey),
+    OAuthToken(OAuthToken)
+}
+
+// These trait impls build an Application variant out of its required types
+impl Into<Application> for AppId {
+    fn into(self) -> Application {
+        Application::AppId(self, None)
+    }
+}
+
+impl Into<Application> for (AppId, AppKey) {
+    fn into(self) -> Application {
+        Application::AppId(self.0, Some(self.1))
+    }
+}
+
+impl Into<Application> for UserKey {
+    fn into(self) -> Application {
+        Application::UserKey(self)
+    }
+}
+
+impl Into<Application> for OAuthToken {
+    fn into(self) -> Application {
+        Application::OAuthToken(self)
+    }
 }
 
 impl Application {
-    pub fn from_app_id(app_id: String) -> Self {
-        Application::AppId(app_id, None)
+    pub fn from_app_id<T: Into<AppId>>(app_id: T) -> Self {
+        Application::AppId(app_id.into(), None)
     }
 
-    pub fn from_app_id_and_key(app_id: String, app_key: String) -> Self {
-        Application::AppId(app_id, Some(app_key))
+    pub fn from_app_id_and_key<T: Into<AppId>, U: Into<AppKey>>(app_id: T, app_key: U) -> Self {
+        Application::AppId(app_id.into(), Some(app_key.into()))
     }
 
-    pub fn from_user_key(user_key: String) -> Self {
-        Application::UserKey(user_key)
+    pub fn from_user_key<T: Into<UserKey>>(user_key: T) -> Self {
+        Application::UserKey(user_key.into())
     }
 
-    pub fn from_oauth_token(token: String) -> Self {
-        Application::OAuthToken(token)
+    pub fn from_oauth_token<T: Into<OAuthToken>>(token: T) -> Self {
+        Application::OAuthToken(token.into())
     }
 }
 
@@ -32,14 +177,14 @@ impl ToParams for Application {
         let mut v = Vec::<(&str, &str)>::with_capacity(2);
         match *self {
             AppId(ref app_id, None) => {
-                v.push(("app_id", app_id));
+                v.push(("app_id", app_id.as_ref()));
             },
             AppId(ref app_id, Some(ref app_key)) => {
-                v.push(("app_id", app_id));
-                v.push(("app_key", app_key));
+                v.push(("app_id", app_id.as_ref()));
+                v.push(("app_key", app_key.as_ref()));
             },
-            UserKey(ref user_key) => v.push(("user_key", user_key)),
-            OAuthToken(ref token) => v.push(("access_token", token)),
+            UserKey(ref user_key) => v.push(("user_key", user_key.as_ref())),
+            OAuthToken(ref token) => v.push(("access_token", token.as_ref())),
         };
 
         v
