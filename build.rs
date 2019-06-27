0243595cd3fe66700f7b88be5cea60a8436e2cdd
Alejandro Martinez Ruiz <amr@redhat.com>
2019-06-27 16:47:47 +0200

    Cargo.toml,build.rs: detect nightly rustc and enable the nightly flag
    This introduces a build.rs step to detect the compiler channel and
    enable hiding unstable/nightly-only features like benchmarks behind
    the nightly feature flag.
    
    This solution is based on:
    https://stackoverflow.com/questions/37674758/ignore-benchmarks-when-using-stable-beta

---
+++ build.rs
@@ -0,0 +1,8 @@
+// Detect nightly compilers.
+use rustc_version::{version_meta, Channel};
+
+fn main() {
+    if version_meta().unwrap().channel == Channel::Nightly {
+        println!("cargo:rustc-cfg=feature=\"nightly\"");
+    }
+}
