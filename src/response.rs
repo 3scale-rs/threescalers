225d819ee6eb08d5731097e39f37e256569eeb7e
David Ortiz <z.david.ortiz@gmail.com>
2019-07-08 15:13:14 +0200

    response: add parsing for metric hierarchies

--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,7 @@
 use chrono::prelude::*;
 use serde::de::{self, Deserializer, MapAccess, Visitor};
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 use std::time::SystemTime;
@@ -17,6 +18,19 @@ impl From<SystemTime> for PeriodTime {
 impl From<PeriodTime> for SystemTime {
     fn from(pt: PeriodTime) -> Self {
         pt.0
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct MetricsHierarchy(HashMap<String, Vec<String>>);
+
+impl MetricsHierarchy {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    pub fn insert(&mut self, parent_metric: String, children_metrics: Vec<String>) {
+        self.0.insert(parent_metric, children_metrics);
     }
 }
 
@@ -45,6 +59,9 @@ pub struct Authorization {
     authorized: bool,
     plan: String,
     usage_reports: UsageReports,
+
+    #[serde(rename = "hierarchy")]
+    metrics_hierarchy: Option<MetricsHierarchy>,
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
@@ -134,6 +151,45 @@ impl<'de> Deserialize<'de> for PeriodTime {
     }
 }
 
+struct MetricsHierarchyVisitor;
+
+impl<'de> Visitor<'de> for MetricsHierarchyVisitor {
+    type Value = MetricsHierarchy;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a structure that represents a hierarchy of metrics")
+    }
+
+    fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+    where
+        V: MapAccess<'de>,
+    {
+        let mut hierarchy = MetricsHierarchy::new();
+
+        // The key in the hierarchy structure is always "metric". It is not
+        // used, but we need to read it to get the value.
+        while let Some(_) = map.next_key::<String>()? {
+            let val: HashMap<String, String> = map.next_value()?;
+
+            let parent_metric = val["name"].to_owned();
+            let children_metrics = val["children"].split(' ').map(|s| s.to_owned()).collect();
+
+            hierarchy.insert(parent_metric, children_metrics);
+        }
+
+        Ok(hierarchy)
+    }
+}
+
+impl<'de> Deserialize<'de> for MetricsHierarchy {
+    fn deserialize<D>(deserializer: D) -> Result<MetricsHierarchy, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_any(MetricsHierarchyVisitor)
+    }
+}
+
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct UsageData {
     max_value: u64,
@@ -184,6 +240,7 @@ mod tests {
         let expected_auth = Authorization {
             authorized: true,
             plan: String::from("App Plan"),
+            metrics_hierarchy: None,
             usage_reports: UsageReports::UsageReports(vec![
                 UsageReport {
                     metric: String::from("products"),
@@ -200,6 +257,112 @@ mod tests {
                     period_end: PeriodTime(Utc.ymd(2019, 7, 1).and_hms(0, 0, 0).into()),
                     max_value: 50,
                     current_value: 0,
+                },
+            ]),
+        };
+
+        assert_eq!(parsed_auth, expected_auth);
+    }
+
+    #[test]
+    fn parse_metrics_hierarchy() {
+        let xml_response = r##"
+        <?xml version="1.0" encoding="UTF-8"?>
+        <status>
+            <authorized>true</authorized>
+            <plan>Basic</plan>
+            <usage_reports>
+                <usage_report metric="parent1" period="day">
+                    <period_start>2016-01-01 00:00:00 +0000</period_start>
+                    <period_end>2016-01-02 00:00:00 +0000</period_end>
+                    <max_value>100</max_value>
+                    <current_value>20</current_value>
+                </usage_report>
+                <usage_report metric="parent2" period="day">
+                    <period_start>2016-01-01 00:00:00 +0000</period_start>
+                    <period_end>2016-01-02 00:00:00 +0000</period_end>
+                    <max_value>100</max_value>
+                    <current_value>10</current_value>
+                </usage_report>
+                <usage_report metric="child1" period="day">
+                    <period_start>2016-01-01 00:00:00 +0000</period_start>
+                    <period_end>2016-01-02 00:00:00 +0000</period_end>
+                    <max_value>100</max_value>
+                    <current_value>10</current_value>
+                </usage_report>
+                <usage_report metric="child2" period="day">
+                    <period_start>2016-01-01 00:00:00 +0000</period_start>
+                    <period_end>2016-01-02 00:00:00 +0000</period_end>
+                    <max_value>100</max_value>
+                    <current_value>10</current_value>
+                </usage_report>
+                <usage_report metric="child3" period="day">
+                    <period_start>2016-01-01 00:00:00 +0000</period_start>
+                    <period_end>2016-01-02 00:00:00 +0000</period_end>
+                    <max_value>100</max_value>
+                    <current_value>10</current_value>
+                </usage_report>
+            </usage_reports>
+            <hierarchy>
+                <metric name="parent1" children="child1 child2" />
+                <metric name="parent2" children="child3" />
+            </hierarchy>
+        </status>
+        "##;
+
+        let parsed_auth = Authorization::from_str(xml_response).unwrap();
+
+        let mut expected_hierarchy = MetricsHierarchy::new();
+        expected_hierarchy.insert(
+            String::from("parent1"),
+            vec![String::from("child1"), String::from("child2")],
+        );
+        expected_hierarchy.insert(String::from("parent2"), vec![String::from("child3")]);
+
+        let expected_auth = Authorization {
+            authorized: true,
+            plan: String::from("Basic"),
+            metrics_hierarchy: Some(expected_hierarchy),
+            usage_reports: UsageReports::UsageReports(vec![
+                UsageReport {
+                    metric: String::from("parent1"),
+                    period: Period::Day,
+                    period_start: PeriodTime(Utc.ymd(2016, 1, 1).and_hms(0, 0, 0).into()),
+                    period_end: PeriodTime(Utc.ymd(2016, 1, 2).and_hms(0, 0, 0).into()),
+                    max_value: 100,
+                    current_value: 20,
+                },
+                UsageReport {
+                    metric: String::from("parent2"),
+                    period: Period::Day,
+                    period_start: PeriodTime(Utc.ymd(2016, 1, 1).and_hms(0, 0, 0).into()),
+                    period_end: PeriodTime(Utc.ymd(2016, 1, 2).and_hms(0, 0, 0).into()),
+                    max_value: 100,
+                    current_value: 10,
+                },
+                UsageReport {
+                    metric: String::from("child1"),
+                    period: Period::Day,
+                    period_start: PeriodTime(Utc.ymd(2016, 1, 1).and_hms(0, 0, 0).into()),
+                    period_end: PeriodTime(Utc.ymd(2016, 1, 2).and_hms(0, 0, 0).into()),
+                    max_value: 100,
+                    current_value: 10,
+                },
+                UsageReport {
+                    metric: String::from("child2"),
+                    period: Period::Day,
+                    period_start: PeriodTime(Utc.ymd(2016, 1, 1).and_hms(0, 0, 0).into()),
+                    period_end: PeriodTime(Utc.ymd(2016, 1, 2).and_hms(0, 0, 0).into()),
+                    max_value: 100,
+                    current_value: 10,
+                },
+                UsageReport {
+                    metric: String::from("child3"),
+                    period: Period::Day,
+                    period_start: PeriodTime(Utc.ymd(2016, 1, 1).and_hms(0, 0, 0).into()),
+                    period_end: PeriodTime(Utc.ymd(2016, 1, 2).and_hms(0, 0, 0).into()),
+                    max_value: 100,
+                    current_value: 10,
                 },
             ]),
         };
