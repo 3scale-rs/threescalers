00db0bbadf59f655fd40a92ef283387790d7dbe3
Alejandro Martinez Ruiz <alex@flawedcode.org>
2020-02-10 22:52:29 +0100

    Cargo.toml,src/encoding: update to percent_encoding 2

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,13 +1,18 @@
 use percent_encoding::{
-    define_encode_set,
     utf8_percent_encode,
-    PATH_SEGMENT_ENCODE_SET,
+    AsciiSet,
+    CONTROLS,
 };
 use std::borrow::Cow;
 
-define_encode_set! {
-    pub APISONATOR_EXTENSION_ENCODE_SET = [PATH_SEGMENT_ENCODE_SET] | { ';', '&', '=', '[', ']' }
-}
+const QUERY_ENCODE_SET: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'#').add(b'<').add(b'>');
+const DEFAULT_ENCODE_SET: &AsciiSet = &QUERY_ENCODE_SET.add(b'`').add(b'?').add(b'{').add(b'}');
+const PATH_SEGMENT_ENCODE_SET: &AsciiSet = &DEFAULT_ENCODE_SET.add(b'%').add(b'/');
+const APISONATOR_EXTENSION_ENCODE_SET: &AsciiSet = &PATH_SEGMENT_ENCODE_SET.add(b';')
+                                                                           .add(b'&')
+                                                                           .add(b'=')
+                                                                           .add(b'[')
+                                                                           .add(b']');
 
 pub fn encode(s: &str) -> Cow<str> {
     utf8_percent_encode(s, APISONATOR_EXTENSION_ENCODE_SET).into()
