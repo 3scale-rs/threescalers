27bdd207ad5e5ea02a8527ccc8f1ed9ffd8b4c9b
Alejandro Martinez Ruiz <alex@flawedcode.org>
2021-07-26 14:59:24 +0200

    encoding: use percent_encode (rather than utf8 version) and make `encode` generic
    This lifts the need to check the strings coming in from the C boundary
    and enables better ergonomics for this function.

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,4 +1,4 @@
-use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+use percent_encoding::{percent_encode, AsciiSet, CONTROLS};
 use std::borrow::Cow;
 
 const QUERY_ENCODE_SET: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'#').add(b'<').add(b'>');
@@ -11,6 +11,6 @@ const APISONATOR_EXTENSION_ENCODE_SET: &AsciiSet = &PATH_SEGMENT_ENCODE_SET
     .add(b'[')
     .add(b']');
 
-pub fn encode(s: &str) -> Cow<str> {
-    utf8_percent_encode(s, APISONATOR_EXTENSION_ENCODE_SET).into()
+pub fn encode<S: AsRef<[u8]> + ?Sized>(s: &S) -> Cow<str> {
+    percent_encode(s.as_ref(), APISONATOR_EXTENSION_ENCODE_SET).into()
 }
