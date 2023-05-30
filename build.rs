a4cf9381c307fb95ff7c42c5bd0c74ddc95c3960
phala <phala@redhat.com>
2023-05-30 13:46:52 +0200

    misc: fix needless borrows in build.rs

--- build.rs
+++ build.rs
@@ -158,7 +158,7 @@ mod autocfg {
             pub fn from_rustc(rustc: &Path) -> Result<Self, Error> {
                 // Get rustc's verbose version
                 let output = Command::new(rustc)
-                    .args(&["--version", "--verbose"])
+                    .args(["--version", "--verbose"])
                     .output()
                     .map_err(error::from_io)?;
                 if !output.status.success() {
@@ -767,7 +767,7 @@ mod autocfg {
 
                     cargo_target_dir
                         .to_str()
-                        .map(|cargo_target_dir| dir.contains(&cargo_target_dir))
+                        .map(|cargo_target_dir| dir.contains(cargo_target_dir))
                 })
             })
             .unwrap_or(false)
